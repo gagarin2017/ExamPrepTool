8365bf35cef579929d48a2f32a9fd8de90cca4e4
gagarin2017 <yury_gagarin2017@mail.ru>
2020-06-22 22:22:08 +0100

    -- adding basic react project

---
+++ exam-prep-tool/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import MyLayout from "./container/Layout/MyLayout";
+
+function App() {
+  return <MyLayout />;
+}
+
+export default App;
---
+++ exam-prep-tool/src/container/Layout/MyLayout.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+import { Layout, Menu, Breadcrumb } from "antd";
+import "antd/dist/antd.css";
+import classes from "./MyLayout.module.css";
+const { Header, Content, Footer } = Layout;
+
+export default class MyLayout extends Component {
+  render() {
+    return (
+      <Layout className="layout">
+        <Header>
+          <div className={classes.logo}> Exam Preparation Tool </div>
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <div
+            style={{
+              background: "#fff",
+              padding: 24,
+              minHeight: 280,
+              marginTop: "45px",
+            }}
+          >
+            Content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2016 Created by Ant UED
+        </Footer>
+      </Layout>
+    );
+  }
+}
